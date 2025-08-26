f56dd00177899f08db6c2b90a00842724ce3062f
zzimini <rlawlals1123@gmail.com>
2025-08-26 16:25:04 +0900

    ✨ feat: buyer_classes 페이지 제작
    ✨ feat: buyer_classes 페이지 제작

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,8 @@ import App from "@src/app/App";
 import MainLandingPage from "@src/domains/main/areas/home/features/main-landing/pages/MainLandingPage";
 import MainClassesPage from "@src/domains/main/areas/home/features/main-landing/pages/MainClassesPage";
 import ClassesDetailPage from "@src/domains/main/areas/home/features/class-detail/pages/ClassesDetailPage";
+import MainStorePage from "@src/domains/main/areas/home/features/main-landing/pages/MainStorePage";
+import StoreClassesPage from "@src/domains/buyer/areas/store/features/main-landing/pages/StoreClassesPage";
 
 import "./index.css";
 
@@ -19,6 +21,8 @@ const router = createBrowserRouter([
       { path: "main", element: <MainLandingPage /> },
       { path: "main/classes", element: <MainClassesPage /> },
       { path: "main/classes/:classId", element: <ClassesDetailPage /> },
+      { path: "main/stores", element: <MainStorePage /> },
+      { path: "stores/:store/classes", element: <StoreClassesPage /> },
     ],
   },
 ]);

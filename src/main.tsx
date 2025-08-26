741e6cf85a6e1c738730c1ab3cc4ff1679ec1efc
zzimini <rlawlals1123@gmail.com>
2025-08-26 11:34:54 +0900

    ✨ feat: classesdetail 페이지 제작
    ✨ feat: classesdetail 페이지 제작

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import App from "./app/App";
 // 페이지들
 import MainLandingPage from "./domains/main/areas/home/features/main-landing/pages/MainLandingPage";
 import MainClassesPage from "./domains/main/areas/home/features/main-landing/pages/MainClassesPage";
+import ClassesDetailPage from "./domains/main/areas/home/features/class-detail/pages/ClassesDetailPage";
 
 // 스타일 (Tailwind)
 import "./routes/index.css";
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
       { index: true, element: <MainLandingPage /> }, // /
       { path: "main", element: <MainLandingPage /> }, // /main
       { path: "main/classes", element: <MainClassesPage /> }, // /main/classes
+      { path: "main/classes/:classId", element: <ClassesDetailPage /> }, // /main/classes/:classId
     ],
   },
 ]);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,3 @@
-// src/routes/index.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -7,18 +6,19 @@ import App from "@src/app/App";
 // 페이지
 import MainLandingPage from "@src/domains/main/areas/home/features/main-landing/pages/MainLandingPage";
 import MainClassesPage from "@src/domains/main/areas/home/features/main-landing/pages/MainClassesPage";
+import ClassesDetailPage from "@src/domains/main/areas/home/features/class-detail/pages/ClassesDetailPage";
 
 import "./index.css";
 
-// 라우트 트리
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { index: true, element: <MainLandingPage /> }, // GET /
-      { path: "main", element: <MainLandingPage /> }, // GET /main
-      { path: "main/classes", element: <MainClassesPage /> }, // GET /main/classes
+      { index: true, element: <MainLandingPage /> },
+      { path: "main", element: <MainLandingPage /> },
+      { path: "main/classes", element: <MainClassesPage /> },
+      { path: "main/classes/:classId", element: <ClassesDetailPage /> },
     ],
   },
 ]);

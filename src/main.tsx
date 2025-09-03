936224a19f1ed6bc953975048a36f652758974e6
jahuiLee <jennysky46@gmail.com>
2025-09-03 12:25:36 +0900

    ♻️ refactor: 메인 페이지 라우팅 mainRoutes로 분리

--- src/main.tsx
+++ src/main.tsx
@@ -6,39 +6,26 @@ import App from '@src/App/App';
 
 // 페이지
 import MainLandingPage from '@src/domains/main/areas/home/features/main-landing/pages/MainLandingPage';
-import MainClassesPage from '@src/domains/main/areas/home/features/main-landing/pages/MainClassesPage';
-import ClassesDetailPage from '@src/domains/main/areas/home/features/class-detail/pages/ClassesDetailPage';
-import MainStorePage from '@src/domains/main/areas/home/features/main-landing/pages/MainStorePage';
 import StoreClassesPage from '@src/domains/buyer/areas/classes/pages/StoreClassesPage';
-import MainClassOrderPage from '@src/domains/main/areas/home/features/class-order/pages/MainClassOrderPage';
-import MainClassOrderResultPage from '@src/domains/main/areas/home/features/class-order/pages/MainClassOrderResultPage';
-import MainMypagePage from '@src/domains/main/areas/mypage/pages/MainMypagePage';
-import MainMypageClassesPage from '@src/domains/main/areas/mypage/pages/MainMypageclasses';
 import './routes/index.css';
 
 import { sellerRoutes } from '@src/domains/seller/routes';
-import { buyerRoutes } from './domains/buyer/routes';
-import loginRoutes from './domains/common/auth/routes';
+import { buyerRoutes } from '@src/domains/buyer/routes';
+import loginRoutes from '@src/domains/common/auth/routes';
+import mainRoutes from '@src/domains/main/routes';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
-      ...loginRoutes,
       { index: true, element: <MainLandingPage /> },
-      { path: 'main', element: <MainLandingPage /> },
-      { path: 'main/classes', element: <MainClassesPage /> },
-      { path: 'main/classes/:classId', element: <ClassesDetailPage /> },
-      { path: 'main/stores', element: <MainStorePage /> },
-      { path: ':store/classes', element: <StoreClassesPage /> },
-      { path: 'main/classes/order', element: <MainClassOrderPage /> },
-      { path: 'main/classes/order/result', element: <MainClassOrderResultPage /> },
-
-      { path: 'main/mypage', element: <MainMypagePage /> },
-      { path: 'main/mypage/classes', element: <MainMypageClassesPage /> },
+      ...loginRoutes,
+      ...mainRoutes,
       ...sellerRoutes,
       ...buyerRoutes,
+      // buyerRoutes
+      { path: ':store/classes', element: <StoreClassesPage /> },
     ],
   },
 ]);

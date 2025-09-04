cb4965c23bf13dd432220ba969f037b992ec74ec
zzimini <rlawlals1123@gmail.com>
2025-09-04 11:53:28 +0900

    💄 style: 래거시랑 css 맞춤
    💄 style: 래거시랑 css 맞춤

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "@src/app/App";
+import App from "@src/App/App";
 
 // 페이지
 import MainLandingPage from "@src/domains/main/areas/home/features/main-landing/pages/MainLandingPage";
